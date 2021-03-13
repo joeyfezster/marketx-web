eccef7f27b6e24a3536a0f966a40e79b825e1d2e
joeyfezster <joey.baruch@gmail.com>
2021-03-13 03:17:44 +0200

    Workaround: login not working, temp group buy page made public

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
-import React from 'react';
-import { Provider } from 'react-redux'
-import { BrowserRouter, Switch } from 'react-router-dom'
-import { PersistGate } from 'redux-persist/integration/react'
-
-import HomePage from 'pages/home/HomePage';
 import SigninPage from 'pages/auth/SigninPage';
 import SignupPage from 'pages/auth/SignupPage';
+import GroupDealPage from 'pages/groupDeal/GroupDealPage';
+import HomePage from 'pages/home/HomePage';
+import React from 'react';
+import { Provider } from 'react-redux';
+import { BrowserRouter, Switch } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/integration/react';
 import AppRoute from 'shared/route/AppRoute';
-import GroupDealPage from 'pages/groupDeal/GroupDealPage'
-import { store, persistor } from './store'
-import { SWRConfig } from 'swr';
-import { fetcher } from 'shared/utils/swrHooks/swrFetcher';
 import routePaths from 'shared/routePaths';
+import { fetcher } from 'shared/utils/swrHooks/swrFetcher';
+import { SWRConfig } from 'swr';
+import { persistor, store } from './store';
+
 
 function App() {
   return (
@@ -27,8 +27,8 @@ function App() {
             <Switch>
               <AppRoute path={routePaths.SIGNIN} component={SigninPage} />
               <AppRoute path={routePaths.SIGNUP} component={SignupPage} />
+              <AppRoute path={routePaths.GROUP_DEAL} component={GroupDealPage} />
               <AppRoute restricted={true} path="/" component={HomePage} />
-              <AppRoute restricted={true} path={routePaths.GROUP_DEAL} component={GroupDealPage} />
             </Switch>
           </BrowserRouter>
         </SWRConfig>
